aa16d1673565282ac33cf250de461840faeb3bb1
selavii <ddurbailo@mail.ru>
2023-10-29 15:00:25 +0200

    adding the file class which will have attributes such as name, extension, createdTime, updatedTime and other common methods

---
+++ oop_lab33/src/main/java/org/example/File.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class File {
+    protected String name;
+    protected String extension;
+    protected LocalDateTime createdTime;
+    protected LocalDateTime updatedTime;
+
+    public File(String name, String extension) {
+        this.name = name;
+        this.extension = extension;
+        this.createdTime = LocalDateTime.now();
+        this.updatedTime = LocalDateTime.now();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public LocalDateTime getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(LocalDateTime updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+}
+
