e4fca23cb1a72590991f223c725eec5699ec8b65
selavii <ddurbailo@mail.ru>
2023-10-29 15:18:40 +0200

    adding the main class, my last class, but something is not working as intended

--- oop_lab33/src/main/java/org/example/Main.java
+++ oop_lab33/src/main/java/org/example/Main.java
@@ -1,19 +1,19 @@
 package org.example;
+import org.example.FileMonitor;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        FileMonitor monitor = new FileMonitor("/path/to/folder");  // Replace with your folder path
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        while (true) {
+            System.out.print("Enter command: ");
+            if (!monitor.processCommand(scanner.nextLine())) {
+                scanner.close();
+                break;
+            }
         }
     }
 }
